9ed82733696a7a0cf9ffe22c075023859bb2ea68
Carlos Maiolino <cmaiolino@orion.maiolino.org>
2011-01-26 17:49:02 -0200

    First commit, added clucreate.sh

--- clucreate.sh
+++ clucreate.sh
@@ -17,7 +17,16 @@ PLATAFORM=$1
 NODES=$2
 NODE_NUM=1
 
+#Find executables
+
 QEMU_IMG=`which qemu-img`
+VIRT_INSTALL=`which virt-install`
+
+#Define MAC ADDRESSSES:
+
+MAC_RHEL4_DEF='52:54:00:aa:e4:0'
+MAC_RHEL5_DEF='52:54:00:aa:e5:0'
+MAC_RHEL6_DEF='52:54:00:aa:e6:0'
 
 # CREATE SNAPSHOTS
 snap_create()
@@ -26,7 +35,7 @@ TEMPLATE=$1
 NODE_NAME=$2
 
 echo Creating node $NODE_NAME...
-$QEMU_IMG create -f qcow2 -o backing_file=$TEMPLATE $SNAPSHOTS_DIR/$NODE_NAME &> /dev/null
+$QEMU_IMG create -f qcow2 -o backing_file=$TEMPLATE $SNAPSHOTS_DIR/$NODE_NAME
 echo Node $NODE_NAME created...
 echo
 }
@@ -37,6 +46,20 @@ snap_delete()
 echo delete_vm
 }
 
+vm_create()
+{
+
+OS=$1
+NODE=$2
+MAC=$3
+$VIRT_INSTALL --name=$OS-node$NODE --ram=1024 --vcpus=2 --import --os-type=linux --os-variant=$OS --disk path=$SNAPSHOTS_DIR/$OS-node$NODE.img,bus=ide -w network=default,mac=$MAC,model=e1000 --vnc --noautoconsole -v --virt-type kvm --video=cirrus
+}
+
+vm_delete()
+{
+echo vm_delete
+}
+
 
 RHEL4()
 {
@@ -44,8 +67,10 @@ echo Setting up a RHEL4 cluster...
 echo
 
 while [ $NODE_NUM -le $NODES ]; do
-	snap_create $RHEL4_TEMPLATE RHEL4-NODE$NODE_NUM.img
+	snap_create $RHEL4_TEMPLATE rhel4-node$NODE_NUM.img
+	vm_create rhel4 $NODE_NUM $MAC_RHEL4_DEF$NODE_NUM 
 	NODE_NUM=$(($NODE_NUM+1))
+	
 done
 }
 
@@ -55,18 +80,20 @@ echo Setting up a RHEL5 cluster...
 echo
 
 while [ $NODE_NUM -le $NODES ]; do
-	snap_create $RHEL5_TEMPLATE RHEL5-NODE$NODE_NUM.img
+	snap_create $RHEL5_TEMPLATE rhel5-node$NODE_NUM.img
+	vm_create rhel5 $NODE_NUM $MAC_RHEL5_DEF$NODE_NUM
 	NODE_NUM=$(($NODE_NUM+1))
 done
 }
 
 RHEL6()
 {
-echo Setting up a RHEL5 cluster...
+echo Setting up a RHEL6 cluster...
 echo
 
 while [ $NODE_NUM -le $NODES ]; do
-	snap_create $RHEL6_TEMPLATE RHEL6-NODE$NODE_NUM.img
+	snap_create $RHEL6_TEMPLATE rhel6-node$NODE_NUM.img
+	vm_create rhel6 $NODE_NUM $MAC_RHEL6_DEF$NODE_NUM
 	NODE_NUM=$(($NODE_NUM+1))
 done
 }
